af504f51a70e21e9e8f95eb0d9c030fd4f726b78
John Söderholm <soederholm.john@gmail.com>
2025-09-29 18:55:36 +0200

    feat: dark mode

--- apps/web/src/routes/_authenticated.lazy.tsx
+++ apps/web/src/routes/_authenticated.lazy.tsx
@@ -1,5 +1,19 @@
 import { createLazyFileRoute, Outlet } from "@tanstack/react-router";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export const Route = createLazyFileRoute("/_authenticated")({
-  component: () => <Outlet />,
+  component: () => (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "6rem",
+          "--sidebar-width-mobile": "6rem",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <Outlet />
+    </SidebarProvider>
+  ),
 });
--- apps/web/src/routes/_authenticated/index.tsx
+++ apps/web/src/routes/_authenticated/index.tsx
@@ -1,18 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
 
 export const Route = createFileRoute("/_authenticated/")({
-  component: () => (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "6rem",
-          "--sidebar-width-mobile": "6rem",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar />
-    </SidebarProvider>
-  ),
+  component: () => <div>Hello "/_authenticated/index"!</div>,
 });

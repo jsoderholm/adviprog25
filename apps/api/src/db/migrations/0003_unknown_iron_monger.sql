3947ce71de99545ce00d660e500c67ace47969b0
Stagge <jonatan.stagge@gmail.com>
2025-10-18 19:15:08 +0200

    Fix: ID for favorites in DB

---
+++ apps/api/src/db/migrations/0003_unknown_iron_monger.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "favorites" ALTER COLUMN "id" ADD GENERATED ALWAYS AS IDENTITY (sequence name "favorites_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1);--> statement-breakpoint
+ALTER TABLE "favorites" ADD COLUMN "place_id" integer NOT NULL;
--- apps/api/src/db/schema/favorites.ts
+++ apps/api/src/db/schema/favorites.ts
@@ -2,7 +2,8 @@ import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { user } from "./auth";
 
 export const favorites = pgTable("favorites", {
-  id: integer("id").primaryKey(),
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  placeId: integer("place_id").notNull(),
   displayName: text("display_name").notNull(),
   dateAdded: timestamp("date_added").defaultNow(),
   userId: text("user_id")

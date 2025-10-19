3a4fa0dc23de173de615c3b6839ec63d1a9a4e15
Elias Holm <eliholm@kth.se>
2025-10-19 21:02:19 +0200

    fix: removed unused account API

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -6,7 +6,6 @@ import favorites from "./routes/favorites/favorites.index";
 import geocode from "./routes/geocode/geocode.index";
 import index from "./routes/index.route";
 import weather from "./routes/weather/weather.index";
-import account from "./routes/account/account.route";
 
 const app = createApp();
 
@@ -16,9 +15,7 @@ const apiRoutes = app
   .route("/", auth)
   .route("/weather", weather)
   .route("/geocode", geocode)
-  .route("/favorites", favorites)
-  .route("/geocode", geocode)
-  .route("/account", account);
+  .route("/favorites", favorites);
 
 configureOpenAPI(apiRoutes);
 
